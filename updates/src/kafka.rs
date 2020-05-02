26c9b4c20515d807ccceed47c434ddfbfeddbc3c
Scot Wilcox <wilcox.scot@gmail.com>
2020-05-02 16:14:45 -0400

    Working on updates

---
+++ updates/src/kafka.rs
@@ -0,0 +1,94 @@
+use futures::StreamExt;
+use log::*;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::{CommitMode, Consumer};
+use rdkafka::message::Message;
+use thiserror::Error;
+use tokio::sync::broadcast::{Sender, error::SendError};
+use super::*;
+
+#[derive(Debug, Error)]
+pub enum UpdateError {
+    #[error("Kafka error: {0}")]
+    KafkaError(#[from] rdkafka::error::KafkaError),
+    #[error("String error: {0}")]
+    StringError(#[from] std::str::Utf8Error),
+    #[error("Message from partition {partition} and offset {offset} has no payload")]
+    PayloadError { partition: i32, offset: i64 },
+    #[error("Message from partition {partition} and offset {offset} has no file key set")]
+    FileError { partition: i32, offset: i64 },
+    #[error("Channel error: {0}")]
+    ChannelError(#[from] SendError<UpdateMessage>)
+}
+
+async fn handle_message<M: Message>(
+    sender: &mut Sender<UpdateMessage>,
+    m: &M,
+) -> Result<(), UpdateError> {
+    let partition = m.partition();
+    let offset = m.offset();
+    let bytes = m
+        .payload()
+        .ok_or(UpdateError::PayloadError { partition, offset })?;
+    let file_bytes = m
+        .key()
+        .ok_or(UpdateError::FileError { partition, offset })?;
+    let file = std::str::from_utf8(file_bytes)?;
+    sender.send(UpdateMessage{
+        file: String::from(file),
+        msg: bytes.to_vec()
+    }).await?;
+    Ok(())
+}
+
+async fn handle_stream(
+    sender: &mut Sender<UpdateMessage>,
+    brokers: &str,
+    group_id: &str,
+    topic: &str,
+) -> Result<(), UpdateError> {
+    let consumer: StreamConsumer<rdkafka::consumer::DefaultConsumerContext> = ClientConfig::new()
+        .set("group.id", group_id)
+        .set("bootstrap.servers", brokers)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create()?;
+
+    consumer.subscribe(&[topic])?;
+
+    // consumer.start() returns a stream. The stream can be used ot chain together expensive steps,
+    // such as complex computations on a thread pool or asynchronous IO.
+    let mut message_stream = consumer.start();
+
+    while let Some(message) = message_stream.next().await {
+        match message {
+            Ok(m) => {
+                if let Err(e) = handle_message(sender, &m).await {
+                    error!("{}", e);
+                }
+                if let Err(e) = consumer.commit_message(&m, CommitMode::Async) {
+                    error!("{}", e);
+                }
+            }
+            Err(e) => {
+                error!("{}", e);
+            }
+        }
+    }
+    Ok(())
+}
+
+pub async fn consume(
+    mut sender: Sender<UpdateMessage>,
+    brokers: String,
+    group_id: String,
+    topic: String,
+) {
+    std::thread::sleep(std::time::Duration::from_secs(30));
+    if let Err(e) = handle_stream(&mut sender, &brokers, &group_id, &topic).await {
+        error!("{}", e);
+    }
+}
---
+++ updates/src/main.rs
@@ -0,0 +1,122 @@
+use futures_util::future::{select, Either};
+use futures_util::{SinkExt, StreamExt};
+use log::*;
+use serde::Deserialize;
+use tokio::sync::{broadcast, broadcast::Receiver};
+use tungstenite::Message;
+use anyhow::Result;
+use std::collections::HashSet;
+
+mod kafka;
+
+#[derive(Deserialize)]
+enum Commands {
+    Subscribe(String),
+    Unsubscribe(String),
+}
+
+#[derive(Debug, Clone)]
+struct UpdateMessage {
+    file: String,
+    msg: Vec<u8>
+}
+
+async fn accept_connection(kafka_rcv: Receiver<UpdateMessage>, stream: tokio::net::TcpStream) {
+    if let Err(e) = handle_connection(kafka_rcv, stream).await {
+        error!("{:?}", e);
+    }
+}
+
+async fn handle_connection(kafka_rcv: Receiver<UpdateMessage>, stream: tokio::net::TcpStream) -> Result<()> {
+    let ws_stream = tokio_tungstenite::accept_async(stream).await?;
+    info!("New connection");
+    let (mut ws_send, mut ws_rcv) = ws_stream.split();
+    let mut channel_fut = kafka_rcv.next();
+    let mut ws_fut = ws_rcv.next();
+    let mut subs = HashSet::new();
+    loop {
+        trace!("Going to select");
+        match select(ws_fut, channel_fut).await {
+            Either::Left((ws_msg, channel_fut_continue)) => {
+                trace!("Selected websocket");
+                match ws_msg {
+                    Some(msg) => {
+                        let msg = msg?;
+                        match msg {
+                            Message::Ping(msg) => {
+                                trace!("Ping");
+                                ws_send.send(Message::Pong(msg)).await?;
+                            }
+                            Message::Pong(msg) => {
+                                trace!("Pong");
+                                ws_send.send(Message::Ping(msg)).await?;
+                            }
+                            Message::Close(_) => {
+                                debug!("Close message received, breaking");
+                                break;
+                            }
+                            Message::Text(sub_msg) => match serde_json::from_str(&sub_msg) {
+                                Ok(Commands::Subscribe(filename)) => {
+                                    info!("New subscribe cmd for {:?}", filename);
+                                    subs.insert(filename);
+                                }
+                                Ok(Commands::Unsubscribe(filename)) => {
+                                    info!("New unsubscribe cmd for {:?}", filename);
+                                    subs.remove(&filename);
+                                }
+                                Err(e) => error!("Invalid JSON: {:?}", e)
+                            },
+                            _ => {
+                                error!("Unexpected message {:?} received from client", msg);
+                                ws_send
+                                    .send(Message::Text(format!("Unexpected message {:?}", msg)))
+                                    .await?
+                            }
+                        }
+                    }
+                    None => break,
+                }
+                channel_fut = channel_fut_continue;
+                ws_fut = ws_rcv.next();
+            }
+            Either::Right((channel_msg, ws_fut_continue)) => {
+                trace!("Selected channel");
+                match channel_msg {
+                    Some(ws_msg) => {
+                        let ws_msg = ws_msg?;
+                        if subs.contains(&ws_msg.file) {
+                            debug!("Got message from channel, passing on");
+                            ws_send.send(Message::Binary(ws_msg.msg)).await?;
+                        }
+                    }
+                    None => {
+                        debug!("Got None, breaking");
+                        break;
+                    }
+                }
+                ws_fut = ws_fut_continue;
+                channel_fut = kafka_rcv.next();
+            }
+        }
+    }
+    info!("Closing connection");
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    env_logger::init();
+    let run_url = std::env::var("RUN_URL").unwrap().parse().unwrap();
+    let broker = std::env::var("BROKER").unwrap();
+    let group = std::env::var("GROUP").unwrap();
+    let topic = std::env::var("TOPIC").unwrap();
+    let mut server = tokio::net::TcpListener::bind(&run_url).await.unwrap();
+    info!("Listening for updates");
+    let (channel_send, channel_rcv) = broadcast::channel(100);
+    tokio::spawn(kafka::consume(channel_send, broker, group, topic));
+    while let Ok((stream, _)) = server.accept().await {
+        tokio::spawn(accept_connection(channel_rcv.clone(), stream));
+    }
+    Ok(())
+}
