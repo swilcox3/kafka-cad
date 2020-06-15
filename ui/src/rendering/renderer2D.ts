f43432c68eea71255412d4cd9f0499c6ecb39cf7
Scot Wilcox <wilcox.scot@gmail.com>
2020-06-15 08:30:15 -0400

    Getting webpack-dev-server in

--- ui/src/rendering/renderer2D.ts
+++ ui/src/rendering/renderer2D.ts
@@ -4,8 +4,12 @@ var renderer2d: any = null;
 
 export function initialize(canvas: string) {
     renderer2d = new fabric.Canvas(canvas);
+    fabric.Object.prototype.transparentCorners = false;
     renderer2d.setBackgroundColor("gray", renderer2d.renderAll.bind(renderer2d));
     window.addEventListener('resize', resizeCanvas, false);
+    renderer2d.on('mouse:down', function (options) {
+        console.log(options.target);
+    });
 
     function resizeCanvas() {
         renderer2d.setHeight(document.getElementById('wrapper').offsetHeight);
@@ -31,7 +35,7 @@ export function createSheet(name: string, id: string, width: number, height: num
 
 export function createViewport(sheet_id: string, view_id: string, posX: number, posY: number, scale: number) {
     var viewport = new fabric.Group([], {
-        left: posX, top: posY, originX: "left", originY: "top"
+        left: posX, top: posY
     });
     viewport.id = view_id;
     viewport.sheet_id = sheet_id;
@@ -60,8 +64,8 @@ export function test() {
     console.log("Are we not cached?");
     const id = "test id 1";
     createSheet("Test Sheet", id, 2000, 1000);
-    createViewport(id, "view id", 10, 10, 1);
-    var rect = new fabric.Rect({ width: 100, height: 100, left: 10, top: 10, fill: "red" });
+    createViewport(id, "view id", 100, 100, 1);
+    var rect = new fabric.Rect({ width: 100, height: 100, left: 0, top: 0, fill: "red" });
     addObjectRep(rect);
     console.log(renderer2d);
     renderer2d.requestRenderAll();
